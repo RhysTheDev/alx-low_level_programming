94292c8afc662c7f788d36712bf3e4cdc6919fad
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-11 16:46:07 -0400

    Fix error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,19 +12,19 @@ void jack_bauer(void)
 	int f_minute;
 	int s_minute;
 
-	for(f_hour = 0; f_hour < 3; f_hour++)
+	for (f_hour = 0; f_hour < 3; f_hour++)
 	{
-		for(s_hour = 0; s_hour < 4; s_hour++)
+		for (s_hour = 0; s_hour < 4; s_hour++)
 		{
-			for(f_minute = 0; f_minute < 6; f_minute++)
+			for (f_minute = 0; f_minute < 6; f_minute++)
 			{
-				for(s_minute = 0; s_minute < 10; s_minute++)
+				for (s_minute = 0; s_minute < 10; s_minute++)
 				{
-					_putchar('0'+f_hour);
-					_putchar('0'+s_hour);
+					_putchar('0' + f_hour);
+					_putchar('0' + s_hour);
 					_putchar(':');
-					_putchar('0'+f_minute);
-					_putchar('0'+s_minute);
+					_putchar('0' + f_minute);
+					_putchar('0' + s_minute);
 					_putchar('\n');
 				}
 			}
