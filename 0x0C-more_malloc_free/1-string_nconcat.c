3fdece29ab22ea7710b3cfbec395d83c503e4f92
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-13 16:16:52 -0400

    Fix error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,16 +19,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len = 0;
 
 	if (s1 != NULL)
+	{
 		for (i = 0; s1[i] != '\0'; i++)
 			len++;
+	}
 	len1 = len;
 	if (s2 != NULL)
+	{
 		if (sizeof(s2) < n)
 			for (i = 0; s2[i] != '\0'; i++)
 				len++;
 		if (sizeof(s2) > n)
 			for (i = 0; i < n; i++)
 				len++;
+	}
 
 /* allocate memory for s3 */
 		s3 = malloc((sizeof(char) * (len + 1)));
@@ -39,9 +43,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		s3[i] = s1[i];
 	for (; i < len; i++)
+	{
 		s3[i] = s2[i - len1];
-			s3[len] = '\0';
-
+		s3[len] = '\0';
+	}
 	return (s3);
-
 }
