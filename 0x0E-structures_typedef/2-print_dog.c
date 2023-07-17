6d740956aeb8437f47a0187dff48e7edb2e652db
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-17 10:38:17 -0400

    Initial commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,16 +8,15 @@
   *
   * Return: NULL
   */
-
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
-		d = malloc(sizeof(struct dog));
-		printf("%s", "Name: (nil)");
-	}
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
-}
+		return;
 
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
