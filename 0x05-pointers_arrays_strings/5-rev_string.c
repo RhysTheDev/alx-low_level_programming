80840861dd955d03380a4a5321dcbff722a167e9
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-17 03:00:09 -0400

    Initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, len, last_element;
+	char c;
+	len = 0;
+
+	// get lenght of string
+
+	while (s[len] != '\0')
+		len++;
+
+	last_element = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[last_element];
+		s[last_element--] = c;
+	}
+
+}
+
