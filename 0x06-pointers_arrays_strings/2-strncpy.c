4cc5864d5bba05a79e188ac6d13fa6d7e8c94952
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-17 16:20:48 -0400

    Initial commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-        *(dest + i) = src[i];
+	for (i = 0; i < n; i++)
+		*(dest + i) = src[i];
 
-    return (dest);
+	return (dest);
 }
 
