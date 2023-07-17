2e7269856ad2602d001877a9a03eb314c826d034
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-17 02:32:55 -0400

    Initial commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: pointer to character
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		++s;
+		++len;
+	}
+
+	while (len != -1)
+	{
+		_putchar(*s);
+		--s;
+		--len;
+	}
+}
+
