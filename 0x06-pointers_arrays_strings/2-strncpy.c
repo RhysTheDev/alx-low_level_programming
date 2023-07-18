143f531a3534a4eff37a144641837005aa858aa9
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-18 03:25:29 -0400

    Initial commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,31 +7,17 @@
  *
  * Return: pointer to string
  */
-#include "main.h"
-/**
- * _strncpy - copies a string
- * @dest: pointer to destination str
- * @src: pointer to source str
- * @n: integer
- *
- * Return: pointer to string
- */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	int slen = 0;
-	int start = 0;
+	int i;
 
-	while (src[slen] != '\0')
-		slen++;
-
-	if (slen < n)
-	{
-		for (i = 0; i < n; i++)
-			*(dest + i) = src[i];
-		for (start = i; start < slen - i; start++)
-			*(dest + start) = '\0';
+	for (i = 0; i < n; i++)
+		*(dest + i) = src[i];
+	while(i < n)
+	{	*(dest + i) = '\0';
+		i++;
 	}
+
 	return (dest);
 }
 
