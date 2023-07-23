7aed7de05d96eb3b5dc161fe90db9de4068a2837
RhysTheDev <rhysmwangi96@gmail.com>
2023-07-23 09:46:47 -0400

    Initial commit

---
+++ 0x06-pointers_arrays_strings/6-cap_strings.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - Capitalize all words of a string
+ * @str: string
+ *
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	char *sloc = str;
+
+	if (*sloc >= 97 && *sloc <= 122)
+		*sloc -= 32;
+
+	while (*sloc != '\0')
+	{
+		if (*sloc == ' ' && *(sloc + 1) >= 97 && *(sloc + 1) <= 122)
+			*(sloc + 1) -= 32;
+		sloc++;
+	}
+
+	return (str);
+}
+
