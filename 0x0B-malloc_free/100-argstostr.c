b981220222dec1a8b209bcdc497a611b9ca7d5bd
RhysTheDev <rhysmwangi96@gmail.com>
2023-08-01 16:52:39 -0400

    Initial commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,45 +12,35 @@
 char *argstostr(int ac, char **av)
 {
 	char *output;
-	int c = 0;
 	int i = 0;
 	int j = 0;
+	int c = 0;
 	int ia = 0;
 
-	if (ac == 0)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		if (av[i] == NULL)
-			return (NULL);
-
 		for (j = 0; av[i][j] != '\0'; j++)
+			ia++;
+		ia++;
+	}
+	output = malloc(sizeof(char) * (ia + 1));
+	if (output == NULL)
+		return (NULL);
+
+	c = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			output[c] = av[i][j];
 			c++;
+		}
+		output[c] = '\n';
 		c++;
 	}
-
-	output = malloc((c + 1) * sizeof(char));
-
-	if (output == NULL)
-	{
-		free(output);
-		return (NULL);
-	}
-
-	for (i = j = ia = 0; ia < c; j++, ia++)
-	{
-		if (av[i][j] == '\0')
-		{
-			output[ia] = '\n';
-			i++;
-			ia++;
-			j = 0;
-		}
-		if (ia < c - 1)
-			output[ia] = av[i][j];
-	}
-	output[ia] = '\0';
-
 	return (output);
 }
+
