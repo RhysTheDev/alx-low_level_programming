54d3773d814de4c6d4e13ce1504339858a29f41f
RhysTheDev <rhysmwangi96@gmail.com>
2023-08-01 02:27:50 -0400

    Initial commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: pointer to list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *previous_node;
+
+	while (head)
+	{
+		previous_node = head;
+		head = head->next;
+		free(previous_node);
+	}
+	free(head);
+}
+
