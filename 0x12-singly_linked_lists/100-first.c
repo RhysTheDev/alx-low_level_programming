f0c42bed0c862d7a84729e16d0b1d56fefee14b2
RhysTheDev <rhysmwangi96@gmail.com>
2023-08-02 03:05:40 -0400

    Initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void print_be4_main(void) __attribute__((constructor));
+
+/**
+ * print_b4_main - print messages  before main is executed
+ */
+
+void print_be4_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
