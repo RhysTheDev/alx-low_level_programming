3c273009d45fe138bce2760899bda286102cf796
RhysTheDev <rhysmwangi96@gmail.com>
2023-08-02 02:40:14 -0400

    Fix typo error

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,14 +8,14 @@
  */
 void free_list(list_t *head)
 {
-	list_t *nxt;
+	list_t *next;
 
 	while (head)
 	{
-		nxt = head->nxt;
+		next = head->next;
 		free(head->str);
 		free(head);
-		head = nxt;
+		head = next;
 	}
 }
 
