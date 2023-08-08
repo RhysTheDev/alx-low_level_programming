c41a5d8065dcbbf9fcbce36b0a4b6af6abb0c528
RhysTheDev <rhysmwangi96@gmail.com>
2023-08-08 17:15:10 -0400

    Inital commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ * compare - compares two strings
+ * @first: pointer to first str
+ * @second: pointer to second str
+ *
+ * Return: 0 if false; 1 if true
+ */
+int compare(char *first, char *second)
+{
+	while (*first && *second)
+	{
+		if (*first != *second)
+			return (0);
+		first++;
+		second++;
+	}
+	return (1);
+}
+
+
+/**
  * _strstr - locates a substring
  * @haystack: pointer to str
  * @needle: pointer to substr being located
@@ -8,27 +28,12 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int lneedle = 0;
-	int i = 0;
-	int j = 0;
-
-	while(needle[lneedle])
-		lneedle++;
-
-	while (haystack[i])
+	while (*haystack != '\0')
 	{
-		if (needle[j] != haystack[i])
-			i++;
-		else if (needle[j] == haystack[i])
-		{
-			j++;
-			i++;
-		}
-		else
-			i++;
+		if ((*haystack == *needle) && compare(haystack, needle))
+			return (haystack);
+		haystack++;
 	}
-	if (j == lneedle)
-		return (&haystack[i - j]);
 	return (NULL);
 }
 
