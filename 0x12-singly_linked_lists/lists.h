425c76a51a95ecd39990c6a655701f19baf4531a
RhysTheDev <rhysmwangi96@gmail.com>
2023-09-21 18:45:47 +0300

    Add more solutions

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -32,6 +32,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void print_b4_main(void);
+void _printMessage(void)
 
 
 #endif
