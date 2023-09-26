6796e130549adb80a58113f276f778fd6690bb66
RhysTheDev <rhysmwangi96@gmail.com>
2023-09-26 22:58:08 +0300

    Fix error

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,39 +1,23 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at the end of the list
+ * add_nodeint - adds a new node at the beginning of a list
  * @head: pointer to the list
  * @n: int to be added
  * Return: address of the node
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *temp;
 	listint_t *new_node;
 
-	if (head == NULL)
-		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (*head == NULL)
 	{
-		*head = new_node;
-		return (*head);
+		return (NULL);
 	}
-	temp = *head;
-
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new_node;
-
-	return (new_node);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
 }
-
 
