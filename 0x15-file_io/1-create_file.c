e2645bf36fe61b2fd36ca09299f7af0f84657803
RhysTheDev <rhysmwangi96@gmail.com>
2023-10-02 22:38:32 +0300

    Fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
-  * create_file- creates a file
+  * _strlen - calculate length of a string
+  * @s: string
+  *
+  * Return: string length
+  */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (s[i])
+                i++;
+
+        return (i);
+}
+
+
+/**
+  * create_file - creates a file
   * @filename: pointer to string
   * @text_content: pointer to a string
   *
-  * Return: 1 on success; -1 otherwise
+  * Return: 1 on success; else -1
   */
 
 int create_file(const char *filename, char *text_content)
